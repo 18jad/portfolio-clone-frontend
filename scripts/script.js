374197953c634f1deac3b12f946fb149084db514
Jad Yahya <jadoyahya@gmail.com>
2022-09-12 15:59:16 +0300

    Added form input check

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,11 @@
 const header = document.querySelector("header");
+const submitButton = document.querySelector(".submit-btn");
+const nameInput = document.getElementById("name-input");
+const emailInput = document.getElementById("email-input");
+const telInput = document.getElementById("tel-input");
+const messageInput = document.getElementById("message-input");
+
+const inputs = [nameInput, emailInput, telInput, messageInput];
 
 document.addEventListener("scroll", (e) => {
   if (window.scrollY > 20) {
@@ -6,4 +13,19 @@ document.addEventListener("scroll", (e) => {
   } else {
     header.classList.remove("scroll");
   }
+});
+
+inputs.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    if (
+      nameInput.value.trim().length >= 3 &&
+      emailInput.value.trim() != "" &&
+      telInput.value.trim() != "" &&
+      messageInput.value.trim().length > 5
+    ) {
+      submitButton.disabled = false;
+    } else {
+      submitButton.disabled = true;
+    }
+  });
 });
